8ada232594755ba36ce329721b5ed7b34b7a0da8
beloha <belohadk97@gmail.com>
2017-07-19 21:55:25 +0300

    Add moving circle, add click on it, but not working click on the background and moving circle after it, till

--- wgl/wgl/Scripts/dev/Index.js
+++ wgl/wgl/Scripts/dev/Index.js
@@ -25,9 +25,15 @@
         if (index > 1) {
             for (var i = hideRemember; i <= index; i += 1) {
                 children_h4.eq(i).delay(500 * Math.log(i * Math.pow(2.71, i))).hide("slow");
-                console.log(i);
             }
-            hideRemember = index + 1;
+            hideRemember = index + 1;     
+        }
+
+        // Animate background
+        if (index === 1) {
+            (function (index) {
+                hideAndShow($("#wgl_backround"), 1);
+            })(index);
         }
     })
         .promise()
@@ -37,19 +43,61 @@
                 Uncomment that, to hide 'Hellow message and show main page'
             */
             
-            //// Hide 'Hellow message'
-            //wgl.fadeOut("fast").promise().done(function () {
+            // Hide 'Hellow message'
+            wgl.fadeOut("fast").promise().done(function () {
 
-            //    // Show main page block's
-            //    $.each($(".body-content.container > .row > div"), function (index, value) {
+                // Show main page block's
+                $.each($(".body-content.container > .row > div"), function (index, value) {
 
-            //        $(this).fadeIn("fast");
-            //        $(this).delay(500 * Math.log(index * Math.pow(2.71, index))).fadeIn("slow");
-            //    });
-            //})
+                    $(this).fadeIn("fast");
+                    $(this).delay(500 * Math.log(index * Math.pow(2.71, index))).fadeIn("slow");
+                });
+            })
         });
 
-    
+    /*
+        Summary:
+            Hide and show
+    */
+    function hideAndShow(elem, index) {
+
+        if (elem.css("opacity") > 0) { // DEVELOPMENT: DEBUG BACKGROUND - && false
+            $(elem).delay(2250).animate({
+                opacity: 0
+            }, 1000).promise().done(function () {
+                // Stop loop
+                if (index === 0) {
+                    return;
+                }
+
+                hideAndShow(elem, index -= 1);
+            });
+        } else if (elem.css("opacity") == 0) {
+            $(elem).animate({
+                opacity: 0.8
+            }, 500, function () {
+                /*
+                    Summary:
+                        Background. Show for a few second
+                */
+
+
+                // Animate border
+                $(elem[0]).css("border-top", "0px solid whitesmoke").animate({
+                    borderTopWidth: "2px",
+                    borderLeftWidth: "2px",
+                }, 750);
+            }).promise().done(function () {
+
+                // Stop loop
+                if (index === 0) {
+                    return;
+                }
+
+                hideAndShow(elem, index -= 1);
+            });
+        }
+    }
 })($);
 
 
@@ -63,6 +111,105 @@
     var userX = $(document).outerHeight();
     // SET: Height of each row
     $(".body-content.container > .row ").innerHeight(userX / 3 + "px");
+})($);
+
+
+/*--
+    Summary: Nav block
+--*/
+(function ($) {
+
+    var nav = $("#nav"), navY,
+
+        // Variable: For circle
+        circle, circleX, circleY, direction = true,
+        circleHighestPosition, circleLowestPosition, circleMoveInterval;
+
+
+    var variableInit = setInterval(function () {
+
+        if ($(nav).is(":visible")) {
+            // GET: Nav measurements
+            navY = $(nav).innerHeight();
+
+            // Mathematical calculation: For circle
+            circle = $(nav).children(".circle");
+            circleX = $(circle).position().left;
+            circleY = $(circle).position().top;
+            circleHighestPosition = navY - $(circle).height() - 15;
+            circleLowestPosition = $(nav).offset().top + 10;
+
+            // Circle move logic
+            circleMoveInterval = setInterval(CircleMove, 200);
+
+            // Stop circle while hover, and resue moving while mouse is left it
+            $(circle).hover(function (e) {
+
+                clearInterval(circleMoveInterval); 
+            }, function (e) {
+
+                // Don't change circle position, if we click on it
+                if ($(circle).is(":visible")) {
+                    circleMoveInterval = setInterval(CircleMove, 200);
+                }
+            });
+
+            $(circle).on('click', function (e) {
+
+                // Hide circle
+                if ($(circle).is(":visible")) {
+                    $(circle).css("display", "none");
+                }
+
+                // Show hide nav
+                $("#hideNav").fadeIn("fast").on("click", function (e) { // And when click on background - show circle
+                    e.stopPropagation();
+                    e.preventDefault();
+
+                    $(this).fadeOut("slow");
+
+                    // Show circle
+                    $(circle).css("display", "block");
+                    //circleMoveInterval = setInterval(CircleMove, 200); // TODO: Some problem, when trying to move circle
+                });
+            });
+
+            // Stop interval, that init our value - need to init at one time
+            clearInterval(variableInit);
+        }
+    }, 50);
+
+    function CircleMove() {
+
+        // Not run while we don't see nav block
+        if (!$(nav).is(":visible")) {
+            return;
+        }
+
+        switch (direction) {
+            case true:
+                if (circleY >= circleHighestPosition) {
+                    direction = false; // Change direction to reverse
+                } else {
+                    circleY += 10;
+                }
+
+                break;
+            case false:
+                if (circleY <= circleLowestPosition) {
+                    direction = true; // Change direction
+                } else {
+                    circleY -= 10;
+                }
+
+                break;
+        }
+
+        // Change circle position
+        $(circle).animate({
+            top: circleY
+        }, 10); // To change circle move - change this line and interval, that call this function
+    }
 })($);
 
 /*--
